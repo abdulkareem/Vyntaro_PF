bb1010b91cd68195e50d6d8ddd17542834af19bc
abdulkareem <abdulkareem.t@gmail.com>
2026-02-12 15:51:00 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const path = require("path");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.alias["@"] = path.resolve(__dirname, "app");
+  webpack(config) {
+    config.resolve.alias["@"] = path.resolve(__dirname);
     return config;
   }
 };
